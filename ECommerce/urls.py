c44be346b6c2b305d6c089e26558622b4c604901
Nikesh <nikeshvarma50@gmail.com>
2020-07-14 16:35:33 +0530

    Basic Model

--- ECommerce/urls.py
+++ ECommerce/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('HOME.urls')),
     path('admin/', admin.site.urls),
+    path('account/', include('ACCOUNTS.urls')),
 ]
