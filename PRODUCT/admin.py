7d7ae06be1c51d887df84d90b3c630fa78ae1442
Nikesh <nikeshvarma50@gmail.com>
2020-07-16 19:47:55 +0530

    1.0.1

--- PRODUCT/admin.py
+++ PRODUCT/admin.py
@@ -1,12 +1,2 @@
 from django.contrib import admin
 from .models import *
-
-
-@admin.register(Prouduct)
-class ProductAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(ProductCategory)
-class ProductCategoryAdmin(admin.ModelAdmin):
-    pass
--- PRODUCT/models.py
+++ PRODUCT/models.py
@@ -2,9 +2,37 @@ from django.db import models
 from SELLER.models import ShopRegistration
 
 
-class Prouduct(models.Model):
-    shop = models.ForeignKey(ShopRegistration, on_delete=models.CASCADE)
+class MasterCategory(models.Model):
+    category_name = models.CharField(max_length=255)
 
 
-class ProductCategory(models.Model):
-    category = models.ForeignKey(Prouduct, on_delete=models.CASCADE)
+class SubCategory(models.Model):
+    parent_category = models.ForeignKey(MasterCategory, on_delete=models.CASCADE)
+    subcategory_name = models.CharField(max_length=200)
+
+
+class Product(models.Model):
+    product_shop = models.ForeignKey(ShopRegistration, on_delete=models.CASCADE)
+    product_master_category = models.ForeignKey(MasterCategory, on_delete=models.CASCADE)
+    product_sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    product_stoke = models.IntegerField()
+    product_MPR = models.IntegerField()
+    product_selling_price = models.IntegerField()
+    seller = models.ManyToManyField(ShopRegistration, related_name='seller_product')
+
+
+# mobile section
+class MobileSpecification(models.Model):
+    brand_name = models.CharField(max_length=100)
+    mobile_name = models.CharField(max_length=100)
+    starting_selling_price = models.BigIntegerField()
+    mobile_display_size = models.FloatField()
+    mobile_battery_capacity = models.IntegerField()
+
+
+class Mobilevariant(models.Model):
+    mobile = models.ForeignKey(MobileSpecification, on_delete=models.CASCADE)
+    RAM = models.IntegerField()
+    storage = models.IntegerField()
+    price_by_variant = models.BigIntegerField()
+    # mobile section end
