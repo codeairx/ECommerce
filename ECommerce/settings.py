8c57cc3086eb079d35e5d0aa71666b1a2d1f8c6c
Nikesh Varma <55392033+nikeshvarma@users.noreply.github.com>
2020-08-02 21:52:14 +0530

    1.0.4

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -112,6 +112,14 @@ AUTH_PASSWORD_VALIDATORS = [
 LOGIN_URL = '/account/login/'
 LOGIN_REDIRECT_URL = '/'
 
+# EMAIL SMTP CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'demo08092000@gmail.com'
+EMAIL_HOST_PASSWORD = 'kgzwcncseclqpfet'
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
--- ECommerce/urls.py
+++ ECommerce/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -25,4 +26,32 @@ urlpatterns = [
                   path('seller/', include('SELLER.urls')),
                   path('user/', include('USER.urls')),
                   path('product/', include('PRODUCT.urls')),
+
+                  # PASSWORD RESET URLS
+                  path(
+                      'account/password-reset/',
+                      auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html'),
+                      name='reset_password'
+                  ),
+
+                  path(
+                      'account/password-reset-done/',
+                      auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+                      name='password_reset_done'
+                  ),
+
+                  path(
+                      'account/password-reset-confirm/<uidb64>/<token>/',
+                      auth_views.PasswordResetConfirmView.as_view(
+                          template_name='accounts/password_reset_confirm_view.html'),
+                      name='password_reset_confirm'
+                  ),
+
+                  path(
+                      'account/password-reset-complete/',
+                      auth_views.PasswordResetCompleteView.as_view(
+                          template_name='accounts/password_reset_complete_view.html'),
+                      name='password_reset_complete'
+                  )
+
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
